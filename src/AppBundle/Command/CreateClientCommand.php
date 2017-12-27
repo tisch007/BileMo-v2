0d27507372e2bcf9360dc4a672ca934ee227e15d
tisch <cyrille.tischenbach@gmail.com>
2017-12-27 18:58:50 +0100

    Update CreateClientCommand.php

--- src/AppBundle/Command/CreateClientCommand.php
+++ src/AppBundle/Command/CreateClientCommand.php
@@ -14,7 +14,6 @@ class CreateClientCommand extends ContainerAwareCommand
         $this
             ->setName('create-new-client')
             ->setDescription('Create a new client')
-            ->addArgument('mail', InputArgument::REQUIRED, 'Your email')
         ;
     }
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -24,29 +23,9 @@ class CreateClientCommand extends ContainerAwareCommand
         $client->setRedirectUris(array($this->getContainer()->get('kernel')->getRootDir()));
         $client->setAllowedGrantTypes(array('password', 'refresh_token'));
         $clientManager->updateClient($client);
-        $message = \Swift_Message::newInstance()
-        ->setSubject('Your BileMo API access code')
-        ->setFrom('noreply@tisch.fr')
-        ->setTo($input->getArgument('mail'))
-            ->setBody(
-                $this->getContainer()->get('templating')->render(
-                    'email.html.twig',
-                    array(
-                        'client_id' => $client->getPublicId(),
-                        'client_secret' => $client->getSecret(),
-                    )
-                ),
-                'text/html'
-            )
-        ;
+        
         $output->writeln('Your BileMo API access code : ');
         $output->writeln('Your client_id : ' . $client->getPublicId());
         $output->writeln('Your client_secret : ' . $client->getSecret());
-
-        if($this->getContainer()->get('mailer')->send($message)){
-            $output->writeln('Your BileMo access code has been sent to your email address.');
-        }else{
-            $output->writeln('error while sending the email.');
-        }
     }
 }
